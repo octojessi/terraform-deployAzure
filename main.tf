68adee06123b088f7ac0b4edb2b001ee2538e35c
Jessi Moths <rhoticity@github.com>
2020-02-12 01:46:46 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "storage-aea"
-    storage_account_name = "testgit01"
-    container_name       = "terraform01"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "terraformactionsdemo"
+    storage_account_name = "actionsdemo28287"
+    container_name       = "terraformactionsdemo"
+    key                  = "mFL1+qCbPMNyNmMbhHQPqnsxx3CBYBJ1shjbobWAQZtfmV0wqKW4J2ntodRImz08uVPgSv34kq3+tbvHI5MHFA=="
   }
 }
 
-resource "azurerm_resource_group" "rsg_app_plan" {
-  name     = "rhoticity-test"
-  location = "US Central"
+resource "azurerm_resource_group" "tf-actions-demo-jjm" {
+  name     = "tf-actions-demo-21220"
+  location = "eastus"
 }
 
 resource "azurerm_app_service_plan" "app_plan_test" {
