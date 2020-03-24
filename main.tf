e62bf5ca26f0f1d112094672264bd2c80a049422
Jessi Moths <rhoticity@github.com>
2020-03-24 15:38:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "rsg_app_plan" {
 }
 
 resource "azurerm_app_service_plan" "app_plan_test" {
-  name                = "hashigithub123123"
+  name                = "hashigithub1231231"
   location            = "${azurerm_resource_group.rsg_app_plan.location}"
   resource_group_name = "${azurerm_resource_group.rsg_app_plan.name}"
   kind = "Linux"
